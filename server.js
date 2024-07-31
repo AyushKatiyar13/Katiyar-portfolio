e917b175473ff15fdd85d06d2832f7fcdfa63494
Ayush Katiyar <149803861+AyushKatiyar13@users.noreply.github.com>
2024-08-01 01:54:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -144,8 +144,15 @@ app.post('/send', (req, res) => {
     });
 });
 
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
