888c12da2280d95ecd4706929469df6bc94997c0
Ayush Katiyar <149803861+AyushKatiyar13@users.noreply.github.com>
2024-08-01 01:58:03 +0530

    Update script.js

--- Public directory/script.js
+++ Public directory/script.js
@@ -124,7 +124,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       console.log("Sending data:", JSON.stringify(jsonData));
 
-      fetch("https://new-portfolio-lfys.onrender.com/send", { // Updated URL to your server
+      fetch("https://new-portfolio-lfys.onrender.com", { // Updated URL to your server
         method: "POST",
         headers: {
           "Content-Type": "application/json",
